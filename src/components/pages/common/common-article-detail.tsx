86d653a91c142ff0f44a273590d6388afd07d211
Jiří Nenáhlo <jiri.nenahlo@gmail.com>
2019-11-22 17:18:54 +0100

    Initial template for article detail page for the author to react to feedback on his new article

--- src/components/pages/common/common-article-detail.tsx
+++ src/components/pages/common/common-article-detail.tsx
@@ -1,16 +1,64 @@
 import * as React from "react";
 import { TemplatePage } from "components/pages/template/template-page";
 import { RouteComponentProps } from "react-router";
+import { Table, Button } from "react-bootstrap";
 
 interface RouteProps {
     id: string;
 }
 
+interface Message {
+    id: string;
+    author: string;
+    content: string;
+}
+
 export class ArticleDetail extends React.Component<RouteComponentProps<RouteProps>> {
+
+    messages: Message[] = [
+        { id: "m1", author: "reviewer", content: "Předělej tohle" },
+        { id: "m2", author: "author", content: "OK" },
+        { id: "m3", author: "reviewer", content: "Předělej támhleto" },
+        { id: "m4", author: "author", content: "Hotovo." },
+    ];
 
     content(): JSX.Element {
         return <>
             <h2>Článek #{this.props.match.params.id}</h2>
+
+            <Button variant="primary" className="mt-3 mb-3" onClick={() => this.onDownloadClick()}>
+                Stáhnout poslední verzi (DATUM)
+            </Button>
+            <br />
+            <Button variant="info" className="mt-3 mb-3" onClick={() => this.onAproveClick()}>
+                Schválit
+            </Button>
+
+            <h3>Komentáře</h3>
+            <Table>
+                <tbody>
+                    {this.messages.map((message, index) => {
+                        return <tr key={index} className="row">
+                            {this.tableMessageRow(message)}
+                        </tr>;
+                    })}
+                </tbody>
+            </Table>
+        </>;
+    }
+
+    onDownloadClick() {
+        // TODO: do stuff
+    }
+
+    onAproveClick() {
+        // TODO: do stuff
+    }
+
+    tableMessageRow(message: Message): JSX.Element {
+        return <>
+            <td className="col-md-2"><b>{message.author}</b></td>
+            <td className="col-md">{message.content}</td>
         </>;
     }
 
