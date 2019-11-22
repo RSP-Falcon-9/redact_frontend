f2bceaa79b846609c7534ed81c6bb533a6673171
Jiří Nenáhlo <jiri.nenahlo@gmail.com>
2019-11-22 16:48:59 +0100

    Initial template for new article page

--- src/components/pages/author/author-new-article.tsx
+++ src/components/pages/author/author-new-article.tsx
@@ -1,12 +1,42 @@
 import * as React from "react";
 import { TemplatePage } from "components/pages/template/template-page";
+import { Table, Button } from "react-bootstrap";
 
 export class AuthorNewArticle extends React.Component<{}> {
 
     content(): JSX.Element {
         return <>
             <h2>Nahrát nový článek</h2>
+
+            <Table>
+                <tbody>
+                    <tr>
+                        <td>Název</td>
+                        <td><input type="text" placeholder="" /></td>
+                    </tr>
+                    <tr>
+                        <td>Soubor</td>
+                        <td><input type="file" /></td>
+                    </tr>
+                    <tr>
+                        <td>Autor</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                        <Button variant="primary" className="mt-3 mb-3" onClick={() => this.onUploadNewArticleClick()}>
+                            Nahrát &amp; požádat o recenzi
+                        </Button>
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
         </>;
+    }
+
+    onUploadNewArticleClick() {
+        // TODO: do stuff
     }
 
     render() {
