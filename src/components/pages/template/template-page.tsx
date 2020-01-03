28757ec53c5fb805e52b85fb4479cc01670966e8
Jana Bláhová <48289708+PanJust@users.noreply.github.com>
2020-01-03 10:23:37 +0100

    update template-page.tsx

--- src/components/pages/template/template-page.tsx
+++ src/components/pages/template/template-page.tsx
@@ -14,6 +14,7 @@ export class TemplatePage extends React.Component<Props> {
     render() {
         return (
             <Container className={style.app_container}>
+                <img src="/images/logo.png" width="250" height="250" class="rounded">
                 <Row>
                     <Col sm={6}><h1>Redact</h1></Col>
                     <Col><LoginForm /></Col>
